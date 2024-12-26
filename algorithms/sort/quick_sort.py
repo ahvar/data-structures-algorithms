2170d538c670e629923ed1931a5ce7c662ad327d
Arthur Vargas <arthurvargas@Mac.attlocal.net>
2024-12-26 16:11:04 -0600

    fixed quicksort which was not handling values == to the pivot

--- algorithms/sort/quick_sort.py
+++ algorithms/sort/quick_sort.py
@@ -5,13 +5,14 @@ class Solution:
     def quick_sort(self, arr: List[int]) -> List[int]:
         if len(arr) < 2:
             return arr
-        pivot_index = len(arr) - 1 // 2
+        pivot_index = len(arr) // 2
         pivot = arr[pivot_index]
-        subarr_left = [i for i in arr if i < pivot]
-        subarr_right = [i for i in arr if i > pivot]
-        return self.quick_sort(subarr_left) + [pivot] + self.quick_sort(subarr_right)
+        lesser = [i for i in arr if i < pivot]
+        greater = [i for i in arr if i > pivot]
+        equal = [i for i in arr if i ==  pivot]
+        return self.quick_sort(lesser) + equal + self.quick_sort(greater)
 
 if __name__ == "__main__":
-    arr = [33,10,15,7]
+    arr = [33,43,2,21,10,8,15,7]
     s = Solution()
     rprint(s.quick_sort(arr))
