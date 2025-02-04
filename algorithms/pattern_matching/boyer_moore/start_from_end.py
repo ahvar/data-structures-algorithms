a36bd7e8c96cdc621f307b8689a22fb484130fb9
Arthur Vargas <arthurvargas@Mac.attlocal.net>
2025-02-04 15:01:49 -0600

    pattern matching analysis with boyer-moore algo

---
+++ algorithms/pattern_matching/boyer_moore/start_from_end.py
@@ -0,0 +1,16 @@
+def start_from_end(segment, pattern):
+    i = 0
+    while i < len(segment) - len(pattern) + 1:
+        k = len(pattern) - 1
+        while k > 0 and segment[i + k] == pattern[k]:
+            k -= 1
+        if k == 0:
+            return i
+        i += 1
+    return -1
+
+
+if __name__ == "__main__":
+    pattern = "lmn"
+    segment = "rlnlmnolo"
+    print(start_from_end(segment, pattern))
