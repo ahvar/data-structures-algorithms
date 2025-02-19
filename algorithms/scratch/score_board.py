b018b82c94520250e1beca715c5f907924ee945a
Arthur Vargas <arthurvargas@Mac.attlocal.net>
2025-02-19 15:51:17 -0600

    test case

--- algorithms/scratch/score_board.py
+++ algorithms/scratch/score_board.py
@@ -14,6 +14,9 @@ class GameEntry:
     def __str__(self):
         return "{}: {}".format(self._name, self._score)
 
+    def __eq__(self, other):
+        return self.name == other.name and self.score == other.score
+
 
 class ScoreBoard:
     def __init__(self, capacity):
@@ -29,30 +32,47 @@ class ScoreBoard:
         if self._n == 0:
             self._board[self._n] = entry
             self._n += 1
-        elif self._n > 0:
-            j = 0
-            while j < self._n and self._board[j].score >= entry.score:
-                j += 1
-            if j == self._n:
-                self._board[j] = entry
+        elif self._n == 1:
+            if self._board[0].score >= entry.score:
+                self._board[1] = entry
             else:
-                for k in range(
-                    self._n, j, -1
-                ):  # shift to the right; start immediately after the last score
-                    self._board[k] = self._board[k - 1]
-                self._board[k] = entry
-                self._n += 1
+                self._board[1] = self._board[0]
+                self._board[0] = entry
+            self._n += 1
+        elif self._n > 1:
+            self._insert(entry)
+
+    def _insert(self, entry):
+        j = 0
+        while j < self._n and self._board[j].score >= entry.score:
+            j += 1
+        for k in range(self._n, j, -1):
+            self._board[k] = self._board[k - 1]
+        self._board[j] = entry
+        self._n += 1
 
     def __str__(self):
         return "".join([f"{entry}\n" for entry in self._board])
+
+    @property
+    def count(self):
+        return self._n
+
+    @property
+    def board(self):
+        return self._board
 
 
 if __name__ == "__main__":
     arthur = GameEntry("Arthur", 22)
     carlo = GameEntry("Carlo", 44)
     anna = GameEntry("Anna", 33)
+    mom = GameEntry("Nancy", 55)
+    papa = GameEntry("Manuel", 20)
     score_board = ScoreBoard(10)
     score_board.add(arthur)
     score_board.add(carlo)
     score_board.add(anna)
+    score_board.add(mom)
+    score_board.add(papa)
     print(score_board)
--- algorithms/scratch/test_score_board.py
+++ algorithms/scratch/test_score_board.py
@@ -6,12 +6,16 @@ from score_board import GameEntry, ScoreBoard
 class TestScoreBoard(unittest.TestCase):
 
     def setUp(self):
-        self.arthur = GameEntry("Arthur", 22)
-        self.carlo = GameEntry("Carlo", 44)
-        self.anna = GameEntry("Anna", 33)
+        self.jeb = GameEntry("Jebediah", 5)
+        self.tuna = GameEntry("Tuna", 10)
+        self.arnold = GameEntry("Arnold", 15)
         self.score_board = ScoreBoard(10)
 
     def test_add(self):
-        self.score_board.add(self.arthur)
-        self.score_board.add(self.carlo)
-        self.score_board.add(self.anna)
+        self.score_board.add(self.jeb)
+        self.score_board.add(self.tuna)
+        self.score_board.add(self.arnold)
+        print(self.score_board.board[0])
+        assert self.score_board.board[0] == self.arnold
+        assert self.score_board.board[1] == self.tuna
+        assert self.score_board.board[2] == self.jeb
