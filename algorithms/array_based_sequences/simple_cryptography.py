5efeefe77b160c95f871a8c66196a7a5a9bf5915
Arthur Vargas <arthurvargas@Mac.attlocal.net>
2025-02-21 13:05:57 -0600

    simple encryption with Ceasar cipher

--- algorithms/array_based_sequences/simple_cryptography.py
+++ algorithms/array_based_sequences/simple_cryptography.py
@@ -1,31 +1,61 @@
+"""
+"""
+
+
 class CeasarCipher:
     def __init__(self, rotation):
-        self.rotation = rotation
-        self.alphabet_int_codes = []
-        self.replacements = []
+        self._rotation = rotation
+        self._alphabet_int_codes = []
+        self._replacements = []
 
     def _make_alphabet(self):
         first = ord("A")
         last = ord("Z")
-        while first < last:
-            self.alphabet_int_codes.append(first)
+        while first <= last:
+            self._alphabet_int_codes.append(first)
             first += 1
-        self.alphabet_int_codes.append(last)
 
     def _make_replacements(self):
-        return [
-            self.alphabet_int_codes[(i + self.rotation) % len(self.alphabet_int_codes)]
-            for i in range(len(self.alphabet_int_codes))
+        self._replacements = [
+            self._alphabet_int_codes[
+                (i + self._rotation) % len(self._alphabet_int_codes)
+            ]
+            for i in range(len(self._alphabet_int_codes))
         ]
 
     def transform(self, secret):
+        """
+        1. Convert each character in the secret string to its ordinal value
+        2. Get the index of that value from the list of alphabet integer codes
+        3. Use that index to find the code of the replacement character
+
+        """
         cipher = []
         for c in secret:
             if c.isalpha():
-                cipher.append(self.replacements[self.alphabet_int_codes.index(ord(c))])
-        return ""
+                cipher.append(
+                    chr(self._replacements[self._alphabet_int_codes.index(ord(c))])
+                )
+            else:
+                cipher.append(c)
+        print(cipher)
+        return cipher
+
+    @property
+    def alphabet_int_codes(self):
+        return self._alphabet_int_codes
+
+    @property
+    def replacements(self):
+        return self._replacements
+
+    @property
+    def rotation(self):
+        return self._rotation
 
 
 if __name__ == "__main__":
     cc = CeasarCipher(3)
+    cc._make_alphabet()
+    cc._make_replacements()
     cc.transform("THE EAGLE IS IN PLAY; MEET AT JOE'S")
--- algorithms/array_based_sequences/test_simple_cryptography.py
+++ algorithms/array_based_sequences/test_simple_cryptography.py
@@ -1,17 +1,23 @@
 import unittest
-from simple_cryptography import make_rule, make_alphabet
+from .simple_cryptography import CeasarCipher
 
 
 class TestSimpleCryptography(unittest.TestCase):
 
     def setUp(self):
+        self.cc = CeasarCipher(3)
+        self.cc._make_alphabet()
+        self.cc._make_replacements()
         self.alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
         self.replacements = list("DEFGHIJKLMNOPQRSTUVWXYZABC")
 
     def test_replacement(self):
-        alphabet_int_codes = make_alphabet()
+
         for i in range(len(self.alphabet)):
-            assert chr(alphabet_int_codes[i]) == self.alphabet[i]
-        replacements = make_rule(alphabet_int_codes, 3)
+            assert chr(self.cc.alphabet_int_codes[i]) == self.alphabet[i]
+
         for i in range(len(self.replacements)):
-            assert chr(replacements[i]) == self.replacements[i]
+            assert chr(self.cc.replacements[i]) == self.replacements[i]
+
+    def test_transform(self):
+        self.cc.transform("THE EAGLE IS IN PLAY; MEET AT JOE'S")
