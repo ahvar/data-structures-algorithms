67c48ca20739b742707d13bff0e4aef41a5ba921
Arthur Vargas <arthurvargas@Mac.attlocal.net>
2025-08-07 22:39:44 -0500

    dynamic programming review: longest palindromic substring

--- dynamic_programming/longest_palindromic_substring2.py
+++ dynamic_programming/longest_palindromic_substring2.py
@@ -2,47 +2,36 @@
 Given a string, find the longest substring that is a palindrome
 (reads the same backward as forward).
 
+dp[i][j] means s[i..j]
 
 "aabcdcbefg"
 
 """
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # trivial cases
-        if len(s) == 0: return ""
         n = len(s)
-        if n == 1 or n == 2 and s[0] == s[1]: return s
-
-        # dp[i][j] will be True if s[i...j] is palindromic
-        # so we need an n x n table
-        dp = [[0] * (n) for _ in range(n)]
-        
-        # track the best palindrome seen so far
-        start = 0
-        longest = 1 # every single char is a palindrome of length 1
-
-
-        # Base Case 1: every single character is a palindrome
-        for i in range(1,n): dp[i][i] = True
-
-        # Base Case 2: every single two char substring is a palindrome if the two chars are equal
-        for i in range(n - 1):
+        if n == 0: return ""
+        dp = [[False] * n for _ in range(n)]
+        if n == 1: return s
+        # 1 base case: all single characters are palindromes of length 1
+        for i in range(n): dp[i][i] = True
+        start, max_len = 0, 1
+        # 2 base case: all substrings of length == 2 where characters are equivalent
+        for i in range(1, n):
             if s[i] == s[i+1]:
                 dp[i][i+1] = True
-                start = i
-                longest = 2
-
-        # General Case: substrings of length 3 up to n
-        for length in range(3, n + 1): # loop by substring length so dp[i+1][j-1] is ready
-            for i in range(0, n - length + 1): # start index
-                j = i + length - 1 # end index
-                if s[i] == s[j] and dp[i+1][j-1] == True:
+                start, max_len = i, 2
+        
+        for length in range(3, n + 1):
+            for i in range(0, n-(length+1)):
+                j = i + length - 1
+                # recurrence; char check first; only if ends match do we consult inner substring
+                if s[i] == s[j] and dp[i+1][j-1]:
                     dp[i][j] = True
-                    if length > longest:
-                        start = i
-                        longest = length
-        return s[start:start + longest]
+                    if length > max_len:
+                        start, max_len = i, length
 
+        return s[start:start+max_len]
 
 
 
